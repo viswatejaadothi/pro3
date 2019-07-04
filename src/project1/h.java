3e7655e3420be54ff801b4f9f278fe4becb7393a
viswatejaadothi <52519904+viswatejaadothi@users.noreply.github.com>
2019-07-04 15:46:31 +0530

    Update h.java

--- src/project1/h.java
+++ src/project1/h.java
@@ -3,15 +3,7 @@ package project1;
 public class h {
 	public static void main(String[] args)
 	{
-		System.out.println("teja");
-		System.out.println("leo");
-		System.out.println("ted");
-		System.out.println("adothi");
-		System.out.println("nishi");
-		System.out.println("top");
-	        System.out.println("pop");
-		System.out.println("leo33");
-		System.out.println("l");
+		
 	}
 
 }
